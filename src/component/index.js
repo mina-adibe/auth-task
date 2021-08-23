bed7fab83dfd1e8b9b32766021273b27ad07e940
mina <mina.adibe@gmal.com>
2021-08-23 09:17:28 +0200

    add home

--- src/component/index.js
+++ src/component/index.js
@@ -1,3 +1,4 @@
 //shared
 export { default as Btn } from "./shared/btn/index";
-export { default as TextField } from "./shared/TextField/index";
+export { default as InputField } from "./shared/TextField/index";
+export { default as Navbar } from "./Navbar/index";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Login } from "./login/index";
 export { default as Register } from "./register/index";
+export { default as Home } from "./home/index";
