9d094d35249cb25619353e81da506c282b22f2d4
mina <mina.adibe@gmal.com>
2021-08-26 12:51:25 +0200

    remove receive types from reducers

--- src/store/actionTypes/auth.js
+++ src/store/actionTypes/auth.js
@@ -4,16 +4,12 @@ export const POST_AUTH_LOGIN_RECEIVE = "POST_AUTH_LOGIN_RECEIVE"; // to store th
 
 //Register
 export const POST_AUTH_REGISTER_REQUEST = "POST_AUTH_REGISTER_REQUEST"; // to fire the saga
-export const POST_AUTH_REGISTER_RECEIVE = "POST_AUTH_REGISTER_RECEIVE"; // to store the response
 
 //forget password
 export const POST_AUTH_FORGETPASS_REQUEST = "POST_AUTH_FORGETPASS_REQUEST"; // to fire the saga
-export const POST_AUTH_FORGETPASS_RECEIVE = "POST_AUTH_FORGETPASS_RECEIVE"; // to store the response
 
 //otp
 export const POST_AUTH_OTP_REQUEST = "POST_AUTH_OTP_REQUEST"; // to fire the saga
-export const POST_AUTH_OTP_RECEIVE = "POST_AUTH_OTP_RECEIVE"; // to store the response
 
 //reset password
 export const POST_AUTH_RESETPASS_REQUEST = "POST_AUTH_RESETPASS_REQUEST"; // to fire the saga
-export const POST_AUTH_RESETPASS_RECEIVE = "POST_AUTH_RESETPASS_RECEIVE"; // to store the response
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -4,15 +4,7 @@ const INITIAL_SATE = {};
 
 const AuthReducer = (state = INITIAL_SATE, action) => {
   switch (action.type) {
-    case types.POST_AUTH_REGISTER_RECEIVE:
-      return action.payload;
     case types.POST_AUTH_LOGIN_RECEIVE:
-      return action.payload;
-    case types.POST_AUTH_FORGETPASS_RECEIVE:
-      return action.payload;
-    case types.POST_AUTH_OTP_RECEIVE:
-      return action.payload;
-    case types.POST_AUTH_RESETPASS_RECEIVE:
       return action.payload;
 
     default:
